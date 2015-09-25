97ddab86bf981f38236692a354eeaccb4e822b42
Daniel Martí <mvdan@mvdan.cc>
2015-09-24 22:55:26 -0700

    verify-rewritemeta: use rewritemeta -l
    This keeps it from having to modify the files on metadata/.

--- tools/verify-rewritemeta.sh
+++ tools/verify-rewritemeta.sh
@@ -4,28 +4,12 @@
 
 set -e
 
-err() {
-	echo "$@" >&2
+files=$(fdroid rewritemeta -l)
+
+if [[ -n "$files" ]]; then
+	echo "Run fdroid rewritemeta to fix formatting on these files:"
+	echo "$files"
 	exit 1
-}
-
-has_changed_files() {
-	git update-index -q --refresh
-	git diff-index --quiet HEAD -- && return 1 || return 0
-}
-
-list_changed_files() {
-	git diff-index --name-only HEAD -- || return 0
-}
-
-if has_changed_files; then
-	list_changed_files
-	err "Unstaged changes found; refusing to continue."
 fi
 
-fdroid rewritemeta
-
-if has_changed_files; then
-	list_changed_files
-	err "Run rewritemeta to fix formatting on those files."
-fi
+exit 0
