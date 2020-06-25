11a7290a4888da42bc1835b86b8e40bf25c26323
Hans-Christoph Steiner <hans@eds.org>
2020-06-25 21:21:28 +0200

    gitlab-ci: fix `fdroid build` for new metadata files
    !6845

--- tools/find-changed-builds.py
+++ tools/find-changed-builds.py
@@ -24,27 +24,41 @@ for appid in sorted(changed):
         print(Fore.RED + ('ERROR: %s: %d' % (cmd, p.returncode)) + Style.RESET_ALL,
               file=sys.stderr)
         sys.exit(p.returncode)
-    with open(metadata_file) as fp:
-        previous = yaml.safe_load(fp)
-    cmd = 'git apply'
-    p = subprocess.run(cmd.split(' '), input=diff, stdout=subprocess.DEVNULL)
-    if p.returncode:
-        print(Fore.RED + ('ERROR: %s: %d' % (cmd, p.returncode)) + Style.RESET_ALL,
-              file=sys.stderr)
-        sys.exit(p.returncode)
 
     to_build = []
-    previous_builds = dict()
-    for build in previous['Builds']:
-        previous_builds[build['versionCode']] = build
+    if os.path.exists(metadata_file):
+        with open(metadata_file) as fp:
+            previous = yaml.safe_load(fp)
+        cmd = 'git apply'
+        p = subprocess.run(cmd.split(' '), input=diff, stdout=subprocess.DEVNULL)
+        if p.returncode:
+            print(Fore.RED + ('ERROR: %s: %d' % (cmd, p.returncode)) + Style.RESET_ALL,
+                  file=sys.stderr)
+            sys.exit(p.returncode)
 
-    for build in current['Builds']:
-        vc = build['versionCode']
-        if vc not in previous_builds:
-            to_build.append(vc)
-            continue
-        if build != previous_builds[vc]:
-            to_build.append(vc)
+        previous_builds = dict()
+        for build in previous['Builds']:
+            previous_builds[build['versionCode']] = build
+
+        for build in current['Builds']:
+            vc = build['versionCode']
+            if vc not in previous_builds:
+                to_build.append(vc)
+                continue
+            if build != previous_builds[vc]:
+                to_build.append(vc)
+    else:
+        # this is a brand new metadata file
+        cmd = 'git checkout -- ' + metadata_file
+        p = subprocess.run(cmd.split(' '), stdout=subprocess.DEVNULL)
+        if p.returncode:
+            print(Fore.RED + ('ERROR: %s: %d' % (cmd, p.returncode)) + Style.RESET_ALL,
+                  file=sys.stderr)
+            sys.exit(p.returncode)
+        with open(metadata_file) as fp:
+            data = yaml.safe_load(fp)
+        for build in data['Builds']:
+            to_build.append(build['versionCode'])
 
     for vc in to_build:
         print('%s:%d' % (appid, vc), end=' ')
