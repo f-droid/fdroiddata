438681c7718034b66ad8890b6b68d88894865f2f
Hans-Christoph Steiner <hans@eds.org>
2021-04-15 17:26:22 +0200

    gitlab-ci: check for signatures without build entries

---
+++ tools/check-for-unattached-signatures.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#
+#
+# Apps with reproducible builds include the APK Signature files in the
+# metadata.  If there is no matching entry in Builds:, then those
+# files are useless cruft.
+
+import glob
+import os
+import yaml
+
+errors = 0
+for d in glob.glob('metadata/*/signatures/[0-9]*'):
+    appid = os.path.basename(os.path.dirname(os.path.dirname(d)))
+    with open(os.path.join('metadata', appid + '.yml')) as fp:
+        app = yaml.safe_load(fp)
+    versionCode = os.path.basename(d)
+    found = False
+    for build in app['Builds']:
+        if int(versionCode) == int(build['versionCode']):
+            found = True
+            break
+    if not found:
+        print('ERROR: found signatures files with no matching build:')
+        for f in os.listdir(d):
+            print('\t', os.path.join(d, f))
+
+exit(errors)
