52b802517fc7d172fb54c45bc813464c70cdca52
Hans-Christoph Steiner <hans@eds.org>
2021-04-21 11:05:38 +0200

    gitlab-ci: fix broken logic for running publish when signatures are there
    !8834
    92f77359dd152a9c8e18fd0ec4846fae0e9d9070

--- tools/build-contains-signatures.py
+++ tools/build-contains-signatures.py
@@ -8,17 +8,29 @@ import yaml
 from colorama import Fore, Style
 
 if len(sys.argv) != 2:
-    print(Fore.RED + ('ERROR: incorrect number of arguments') + Style.RESET_ALL)
+    print(
+        Fore.RED
+        + ('ERROR: incorrect number of arguments: ' + ' '.join(sys.argv[1:]))
+        + Style.RESET_ALL
+    )
     exit(1)
 
 appid, versionCode = sys.argv[1].split(':')
 
 if not glob.glob('metadata/%s/signatures/%s/*' % (appid, versionCode)):
-    print(Fore.RED + ('ERROR: no signatures found') + Style.RESET_ALL)
+    print('no signatures found, skipping publish')
     exit(1)
 
 for f in glob.glob('tmp/%s_%s.apk' % (appid, versionCode)):
     os.rename(f, f.replace('tmp/', 'unsigned/'))
 
 if not glob.glob('unsigned/%s_%s.apk' % (appid, versionCode)):
+    print('no unsigned APK found, skipping publish')
     exit(1)
+
+
+print(
+    Fore.GREEN
+    + ('Found signatures for %s, running publish...' % sys.argv[1])
+    + Style.RESET_ALL
+)
