0375e15126acac2a6f72bf35bf5280aad64ddedd
linsui <2873532-linsui@users.noreply.gitlab.com>
2024-11-15 14:55:37 +0800

    tools/merge-autoupdate.sh: add label to bot MRs

--- tools/merge-autoupdate.sh
+++ tools/merge-autoupdate.sh
@@ -7,6 +7,7 @@ cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 glab="glab --repo fdroid/fdroiddata"
 mr_list=$($glab mr list --author checkupdates-bot -F json | jq -r 'map(.iid)[]')
 for mr in $mr_list; do
+  $glab mr update $mr --label "fdroid-bot"
   if [[ $($glab mr view $mr -F json | jq -r '.pipeline.status == "success" and .draft == false') == "true" ]]; then
     echo "Merging $mr..."
     ./tools/auto-merge.sh $mr
