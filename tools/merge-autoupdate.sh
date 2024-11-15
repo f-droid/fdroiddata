ad4cb55d8f3b5136668727887718248eb7899c99
linsui <2873532-linsui@users.noreply.gitlab.com>
2024-11-15 15:35:01 +0800

    tools/merge-autoupdate.sh: reduce api call

--- tools/merge-autoupdate.sh
+++ tools/merge-autoupdate.sh
@@ -7,11 +7,16 @@ cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 glab="glab --repo fdroid/fdroiddata"
 mr_list=$($glab mr list --author checkupdates-bot -F json | jq -r 'map(.iid)[]')
 for mr in $mr_list; do
-  $glab mr update $mr --label "fdroid-bot"
-  if [[ $($glab mr view $mr -F json | jq -r '.title | test("bot: Update CurrentVersion of .*")') == "true" ]]; then
-    $glab mr update --draft
+  mr_stat=$($glab mr view $mr -F json)
+  if [[ $(echo $mr_stat | jq -r '.labels | map(. == "fdroid-bot") | any') == "false" ]]; then
+    $glab mr update $mr --label "fdroid-bot"
   fi
-  if [[ $($glab mr view $mr -F json | jq -r '.pipeline.status == "success" and .draft == false') == "true" ]]; then
+  draft=$(echo $mr_stat | jq -r '.draft')
+  if [[ $(echo $mr_stat | jq -r '.title | test("bot: Update CurrentVersion of .*")') == "true" && $draft == "false" ]]; then
+    $glab mr update --draft
+    draft="true"
+  fi
+  if [[ $(echo $mr_stat | jq -r '.pipeline.status == "success"') == "true" && $draft == "false" ]]; then
     echo "Merging $mr..."
     ./tools/auto-merge.sh $mr
   fi
