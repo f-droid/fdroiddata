a26af23a3fa966cfe008a7aa1e198b4427fb0e3b
linsui <2873532-linsui@users.noreply.gitlab.com>
2024-11-15 14:44:44 +0800

    tools/merge-autoupdate.sh: skip draft

--- tools/merge-autoupdate.sh
+++ tools/merge-autoupdate.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 glab="glab --repo fdroid/fdroiddata"
 mr_list=$($glab mr list --author checkupdates-bot -F json | jq -r 'map(.iid)[]')
 for mr in $mr_list; do
-  if [[ $($glab mr view $mr -F json | jq -r '.pipeline.status') == "success" ]]; then
+  if [[ $($glab mr view $mr -F json | jq -r '.pipeline.status == "success" and .draft == false') == "true" ]]; then
     echo "Merging $mr..."
     ./tools/auto-merge.sh $mr
   fi
