58a9fc1983f4dbbff449ab464887ffae9949a90c
linsui <2873532-linsui@users.noreply.gitlab.com>
2024-11-23 14:14:32 +0800

    tool/merge-autoupdate.sh: increase page size

--- tools/merge-autoupdate.sh
+++ tools/merge-autoupdate.sh
@@ -5,7 +5,7 @@ set -e
 cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 
 glab="glab --repo fdroid/fdroiddata"
-mr_list=$($glab mr list --author checkupdates-bot -F json | jq -r 'map(.iid)[]')
+mr_list=$($glab mr list --author checkupdates-bot --per-page 100 -F json | jq -r 'map(.iid)[]')
 for mr in $mr_list; do
   mr_stat=$($glab mr view $mr -F json)
   if [[ $(echo $mr_stat | jq -r '.labels | map(. == "fdroid-bot") | any') == "false" ]]; then
