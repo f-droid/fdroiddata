7531ad7ffff14f8ff82f54e3ad82484455b61f01
linsui <2873532-linsui@users.noreply.gitlab.com>
2025-05-04 14:28:31 +0800

    tools: auto retry all gitlab queries

--- tools/auto-merge.sh
+++ tools/auto-merge.sh
@@ -6,20 +6,25 @@ cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 
 mr=$1
 glab="glab --repo fdroid/fdroiddata"
+function retry {
+  while output=$("$@"); [[ $? != 0 ]]; do
+    echo $output | grep -q "Source branch does not exist" && break
+    echo $output | grep -q "this merge request has already been merged" && break
+    continue
+  done
+  echo $output
+}
 
 echo "Rebasing..."
-$glab mr rebase $mr --skip-ci
+retry $glab mr rebase $mr --skip-ci
 
 echo "Merging..."
-while $glab mr merge $mr --auto-merge=false --rebase --yes; [[ $? != 0 ]]; do
-  continue
-done
-
+retry $glab mr merge $mr --auto-merge=false --rebase --yes
 echo "Canceling pipelines..."
-merged_commit=$($glab mr view $mr -F json | jq -r 'if .squash then .squash_commit_sha else .sha end')
-head_pipelines=$($glab ci list -F json | jq -r 'map(select(.sha == "'$merged_commit'" and (.source == "push" or .source == "merge_request_event")) | .id)[]')
+merged_commit=$(retry $glab mr view $mr -F json | jq -r 'if .squash then .squash_commit_sha else .sha end')
+head_pipelines=$(retry $glab ci list -F json | jq -r 'map(select(.sha == "'$merged_commit'" and (.source == "push" or .source == "merge_request_event")) | .id)[]')
 for pipeline in $head_pipelines; do
-  glab api --method POST --silent projects/:id/pipelines/$pipeline/cancel
+  retry glab api --method POST --silent projects/:id/pipelines/$pipeline/cancel
 done
 
 echo "Done!"
--- tools/merge-autoupdate.sh
+++ tools/merge-autoupdate.sh
@@ -5,15 +5,23 @@ set -e
 cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 
 glab="glab --repo fdroid/fdroiddata"
-mr_list=$($glab mr list --author checkupdates-bot --per-page 100 -F json | jq -r 'map(.iid)[]')
+function retry {
+  while output=$("$@"); [[ $? != 0 ]]; do
+    continue
+  done
+  echo $output
+}
+
+echo "Fecthing MR list..."
+mr_list=$(retry $glab mr list --author checkupdates-bot --per-page 100 -F json | jq -r 'map(.iid)[]')
 for mr in $mr_list; do
-  mr_stat=$($glab mr view $mr -F json)
+  mr_stat=$(retry $glab mr view $mr -F json)
   if [[ $(echo $mr_stat | jq -r '.labels | map(. == "fdroid-bot") | any') == "false" ]]; then
-    $glab mr update $mr --label "fdroid-bot"
+    retry $glab mr update $mr --label "fdroid-bot"
   fi
   draft=$(echo $mr_stat | jq -r '.draft')
   if [[ $(echo $mr_stat | jq -r '.title | test("bot: Update CurrentVersion of .*")') == "true" && $draft == "false" ]]; then
-    $glab mr update $mr --draft
+    retry $glab mr update $mr --draft
     draft="true"
   fi
   if [[ $(echo $mr_stat | jq -r '.pipeline.status == "success"') == "true" && $draft == "false" ]]; then
